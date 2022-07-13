99376f7232b903978d6e0211a366d3ee8e1db240
igBFranco <igor.franco@aluno.unc.br>
2022-07-13 08:53:12 -0300

    Adding Timer

--- src/components/Footer/Timer/Index.tsx
+++ src/components/Footer/Timer/Index.tsx
@@ -1,19 +1,33 @@
+import { useState } from 'react';
 import styles from './Timer.module.scss';
 
 export default function Timer() {
+    const [count, setCount] = useState<number>(60);
+
+    function counter(time: number = 0) {
+        setTimeout(() => {
+            if(time > 0) {
+                setCount(time - 1);
+                return counter(time - 1);
+            }else {
+                return counter(60);
+            }
+        }, 1000);
+    }
+
     return(
-        <div className={styles.container}>
+        <footer className={styles.container} onLoad={()=>counter(count)}>
             <p className={styles.refresh}>
                 Application refresh in
             </p>
             <div className={styles.timer}>
-                <h2>
-                    600
+                <h2 id="timer">
+                    {count}
                 </h2>
                 <p>
                     seconds
                 </p>
             </div>
-        </div>
+        </footer>
     );
 }
