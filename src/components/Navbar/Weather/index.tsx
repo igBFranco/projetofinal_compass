23dbaf569afee5ec1d8a49d2d8fb67ff417a7e4a
igBFranco <igor.franco@aluno.unc.br>
2022-07-13 15:07:54 -0300

    Location Dinamically

--- src/components/Navbar/Weather/index.tsx
+++ src/components/Navbar/Weather/index.tsx
@@ -1,29 +1,35 @@
 import styles from './Weather.module.scss';
 import cloud from 'assets/cloud.svg';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export default function Weather() { 
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Itaiopolis&units=metric&appid=ab85ba57bbbb423fb62bfb8201126ede";
+    const [lat, setLat] = useState<number>();
+    const [long, setLong] = useState<number>();
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Itaiopolis&units=metric&appid=bf0de12f9df4e3c21f2fb18a7606c041";
     const [city, setCity] = useState<string>();
     const [temp, setTemp] = useState<number>();
     
     
-    function fetchWeather(url: string) {
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                const { main, name } = data;
-                setCity(name);
+    function fetchWeather() {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            let lat = position.coords.latitude;
+            let long = position.coords.longitude;        
+
+            fetch(`https://api.openweathermap.org/data/2.5/weather/?lat=${lat}&lon=${long}&units=metric&APPID=bf0de12f9df4e3c21f2fb18a7606c041`)
+            .then(res => res.json())
+            .then(result => {
+                const { main, name } = result;
+                setCity(`${name} - SC`)
                 setTemp(Math.round(main.temp));
-                }
-            )
+            });
+        });
     }
 
     return(
-        <div className={styles.container} onLoad={()=> fetchWeather(url)} >
+        <div className={styles.container} onLoad={()=> fetchWeather()} >
             <h3>
-                {city} - SC
+                {city}
             </h3>
             <div className={styles.temp}>
                 <img src={cloud} alt="Cloud" />
