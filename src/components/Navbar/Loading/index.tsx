6ec4f9f07794eaaaddad305b0d059406fbc42785
igBFranco <igor.franco@aluno.unc.br>
2022-07-14 16:02:21 -0300

    Layout Fixes

--- src/components/Navbar/Loading/index.tsx
+++ src/components/Navbar/Loading/index.tsx
@@ -2,8 +2,8 @@ import styles from './Loading.module.scss';
 
 export default function Loading() {
     return (
-        <>
+        <div className={styles.loading_container}>
             <div className={styles.loading_spinner}/>
-        </>
+        </div>
     );
 }
--- src/components/Navbar/Weather/index.tsx
+++ src/components/Navbar/Weather/index.tsx
@@ -19,7 +19,7 @@ export default function Weather() {
             .then(res => res.json())
             .then(result => {
                 const { main, name } = result;
-                setCity(`${name} - SC`)
+                setCity(`${name}`)
                 setTemp(Math.round(main.temp));
                 setIsLoading(false);
             });
