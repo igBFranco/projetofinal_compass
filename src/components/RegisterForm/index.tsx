5509ee7167fd0d677dd15b00261f2b857b89ccb7
igBFranco <igor.franco@aluno.unc.br>
2022-07-16 11:01:53 -0300

    Adding User Register with Firebase

--- src/components/RegisterForm/index.tsx
+++ src/components/RegisterForm/index.tsx
@@ -4,10 +4,22 @@ import styles from './RegisterForm.module.scss';
 import { Link, useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { UserContext } from '../../common/Context/User';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from 'firebase-config';
 
 export default function RegisterForm() {
     const navigate = useNavigate();
-    const { emailValid, passValid } = useContext(UserContext);
+    const { email, password, emailValid, passValid } = useContext(UserContext);
+
+    async function register() {
+        try {
+            const user =  await createUserWithEmailAndPassword(auth, email, password);
+            console.log(user);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
 
     return(
         <form className={styles.form}>
@@ -21,8 +33,9 @@ export default function RegisterForm() {
             <button className={styles.button} onClick={(event)=> {
                 event.preventDefault();
                 if(emailValid && passValid){
-                    navigate('/home');
+                    
                 }
+                register();
             }}>
                 Continuar
             </button>
