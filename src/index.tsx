ca11c999b50212aa0605efc7942a6011db71215c
igBFranco <igor.franco@aluno.unc.br>
2022-07-18 13:52:35 -0300

    Adding Mark Pro Font

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
+import './assets/fonts/Mark-Pro.ttf';
+import './assets/fonts/Mark-Pro-Bold.ttf';
 import Router from './routes/routes';
 
 const root = ReactDOM.createRoot(
